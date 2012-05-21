3b6af691dbca8fee230a359228b697ab10d6c492
Remi Gabillet <rgabillet@gmail.com>
2012-05-21 15:10:28 -0700

    make get_words() helper

--- lib/soulmate/helpers.rb
+++ lib/soulmate/helpers.rb
@@ -4,14 +4,18 @@ module Soulmate
   module Helpers
 
     def prefixes_for_phrase(phrase)
-      words = normalize(phrase).split(' ').reject do |w|
-        Soulmate.stop_words.include?(w)
-      end
+      get_words(phrase)
       words.map do |w|
         (MIN_COMPLETE-1..(w.length-1)).map{ |l| w[0..l] }
       end.flatten.uniq
     end
 
+    def get_words(phrase)
+      normalize(phrase).split(' ').reject do |w|
+        Soulmate.stop_words.include?(w)
+      end
+    end
+    
     def normalize(str)
       # str.downcase.gsub(/[^a-z0-9 ]/i, '').strip
       
